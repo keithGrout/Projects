4bbe0c130744694aeccf939a9898ff3e19affb19
Keith <user@Keiths-Macbook-Pro.local>
2013-07-09 19:46:01 -0700

    Initial Commit

---
+++ Chapter 5/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chapter 5
+//
+//  Created by Keith on 7/9/13.
+//  Copyright (c) 2013 Appology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
